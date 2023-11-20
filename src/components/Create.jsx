1c07007961db8a2b07bd08ea85ce34ecbee15225
Tashfeen Chohan <chohantashfeen@gmail.com>
2023-11-20 15:27:08 +0500

    added CREATE POST action

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import { FaPencilAlt, FaUser } from "react-icons/fa";
 import { BiSolidBookContent, BiSolidCategory } from "react-icons/bi";
 import { MdSubtitles } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { createPost } from "../features/postSlice";
 
 const Create = () => {
+  const dispatch = useDispatch()
   const [post, setPost] = useState({
     title: "",
     author: "",
     content: "",
     category: "",
+    date: ""
   });
+
+  const {title, author, content, category} = post;
+  const canSave = Boolean(title) && Boolean(author) && Boolean(content) && Boolean(category)
+
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -19,11 +27,23 @@ const Create = () => {
     });
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch(createPost(post))
+    setPost({
+      title: "",
+      author: "",
+      content: "",
+      category: ""
+    })
+  }
+
+  
   return (
     <div className="flex justify-center items-center min-h-screen w-full">
       <div className="flex justify-center items-center flex-col mt-[-100px] w-[90%] shadow-xl pb-10 md:max-w-xl">
         <h1 className="font-bold text-3xl text-slate-800 my-7">ADD POST</h1>
-        <form className="w-full flex justify-center items-center flex-col">
+        <form onSubmit={handleSubmit} className="w-full flex justify-center items-center flex-col">
           {/* POST TITLE */}
           <div className="flex justify-center items-center gap-5 w-[85%] mx-auto">
             <label htmlFor="title" className="text-xl">
@@ -35,7 +55,7 @@ const Create = () => {
               type="text"
               placeholder="Post Title"
               name="title"
-              value={post.title}
+              value={title}
               onChange={handleChange}
               className="py-[1px] px-2 border-b-2 border-black w-full"
             />
@@ -51,7 +71,7 @@ const Create = () => {
               type="text"
               placeholder="Post Author"
               name="author"
-              value={post.author}
+              value={author}
               onChange={handleChange}
               className="py-[1px] px-2 border-b-2 border-black w-full"
             />
@@ -67,7 +87,7 @@ const Create = () => {
               type="text"
               placeholder="Post Category"
               name="category"
-              value={post.category}
+              value={category}
               onChange={handleChange}
               className="py-1 px-2 border-b-2 border-black w-full"
             />
@@ -84,15 +104,16 @@ const Create = () => {
               placeholder="Post Content"
               cols="30"
               rows="10"
-              value={post.content}
+              value={content}
               onChange={handleChange}
               className="p-1 border border-b-2 border-black w-full h-20 md:h-28"
             ></textarea>
           </div>
 
           <button
-            className="hover:bg-slate-800 mx-auto transition-colors duration-300 bg-slate-700 py-1 px-5 w-[85%] text-white rounded shadow-xl mt-7"
+            className="disabled:bg-slate-600 hover:bg-slate-800 mx-auto transition-colors duration-300 bg-slate-700 py-1 px-5 w-[85%] text-white rounded shadow-xl mt-7"
             type="submit"
+            disabled={!canSave}
           >
             ADD
           </button>
--- src/features/postSlice.jsx
+++ src/features/postSlice.jsx
@@ -1,14 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// CREATE POST ACTION
+export const createPost = createAsyncThunk(
+  "createPost",
+  async (data, { rejectWithValue }) => {
+    try {
+      await axios.post("https://655af2b9ab37729791a85714.mockapi.io/CRUD", data)
+    } catch (error) {
+      rejectWithValue(error.message)
+    }
+  }
+);
 
 export const postSlice = createSlice({
   name: "posts",
   initialState: {
     posts: [],
     loading: false,
-    error: null
+    error: null,
   },
 
-  reducers: {}
-})
+  reducers: {
+
+  },
+
+  extraReducers(builder){
+    builder
+      .addCase(createPost.pending, (state) => {
+        state.loading = true;
+      })
+
+      .addCase(createPost.fulfilled, (state, action) => {
+        state.loading = false;
+        state.posts.push(action.payload)
+      })
+
+      .addCase(createPost.rejected, (state, action) => {
+        state.loading = false
+        state.posts = action.payload
+      })
+  }
+});
 
 export default postSlice.reducer;
